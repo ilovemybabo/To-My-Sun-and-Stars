dd6d3808c75825dc593799792418ac470447bc10
ilovemybabo <xminiminn@gmail.com>
2025-02-12 00:58:16 -0500

    Update shows.js

--- shows.js
+++ shows.js
@@ -1,7 +1,6 @@
-//step 1: get DOM
+// Get DOM elements
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
-
 let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
@@ -12,30 +11,44 @@ thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 3000;
 let timeAutoNext = 7000;
 
-nextDom.onclick = function(){
+// Event listeners for next and prev buttons
+nextDom.onclick = function() {
     showSlider('next');    
 }
 
-prevDom.onclick = function(){
+prevDom.onclick = function() {
     showSlider('prev');    
 }
+
 let runTimeOut;
 let runNextAuto = setTimeout(() => {
     next.click();
-}, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+}, timeAutoNext);
+
+function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
     
-    if(type === 'next'){
+    if(type === 'next') {
         SliderDom.appendChild(SliderItemsDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
         carouselDom.classList.add('next');
-    }else{
+    } else {
         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
         carouselDom.classList.add('prev');
     }
+
+    // Remove any previous animations (reset classes)
+    SliderItemsDom.forEach(item => {
+        item.querySelector('.content').style.animation = 'none';  // Reset animation
+    });
+
+    // Apply animation for the new active item
+    let currentItem = SliderItemsDom[0];  // First item in the list after the change
+    let content = currentItem.querySelector('.content');
+    content.style.animation = 'showContent 0.5s 1s linear forwards';  // Reapply the animation
+
     clearTimeout(runTimeOut);
     runTimeOut = setTimeout(() => {
         carouselDom.classList.remove('next');
@@ -45,5 +58,5 @@ function showSlider(type){
     clearTimeout(runNextAuto);
     runNextAuto = setTimeout(() => {
         next.click();
-    }, timeAutoNext)
+    }, timeAutoNext);
 }
